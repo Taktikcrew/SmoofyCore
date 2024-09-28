ab29973a79044664ff54a0c74cf590ac5c902ba0
Smoofy19 <nxcoop19@gmail.com>
2024-09-28 23:14:08 +0200

    Added example module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,4 @@ include("core-abstraction:adapter-1_8")
 findProject(":core-abstraction:adapter-1_8")?.name = "adapter-1_8"
 include("core-cloud:adapter-polocloud")
 findProject(":core-cloud:adapter-polocloud")?.name = "adapter-polocloud"
+include("core-example")
