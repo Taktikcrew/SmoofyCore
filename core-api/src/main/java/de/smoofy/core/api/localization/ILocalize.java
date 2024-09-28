e5dbc6f578d26123a85f21a63dd1338df3c75fac
Smoofy19 <nxcoop19@gmail.com>
2024-09-28 23:16:42 +0200

    Added documentation

--- core-api/src/main/java/de/smoofy/core/api/localization/ILocalize.java
+++ core-api/src/main/java/de/smoofy/core/api/localization/ILocalize.java
@@ -9,8 +9,21 @@ package de.smoofy.core.api.localization;
 
 public interface ILocalize {
 
+    /**
+     * Load all messages from the given languages.
+     *
+     * @param languages the languages to load
+     */
     void loadLanguages(Language... languages);
 
+    /**
+     * Translate a message from the given language.
+     *
+     * @param language the language to translate the message to
+     * @param key the key of the message
+     * @param replacements the replacements for the message
+     * @return the translated message
+     */
     String translate(Language language, String key, Object... replacements);
 
 }
