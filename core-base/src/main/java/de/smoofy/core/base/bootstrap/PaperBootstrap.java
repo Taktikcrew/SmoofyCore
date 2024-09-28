76f662b75f02d6146094dda0a958e9c8ba6d510b
Smoofy19 <nxcoop19@gmail.com>
2024-09-28 23:17:24 +0200

    Implemented platform bootstrap

--- core-base/src/main/java/de/smoofy/core/base/bootstrap/PaperBootstrap.java
+++ core-base/src/main/java/de/smoofy/core/base/bootstrap/PaperBootstrap.java
@@ -7,6 +7,29 @@ package de.smoofy.core.base.bootstrap;
  * Created - 27.09.24, 23:46
  */
 
-public class PaperBootstrap {
+import de.smoofy.core.base.CoreBase;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
+@Accessors(fluent = true)
+public class PaperBootstrap extends JavaPlugin {
+
+    @Getter
+    @Accessors(fluent = true)
+    private static PaperBootstrap instance;
+
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        new CoreBase(true);
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+    }
 }
--- core-base/src/main/java/de/smoofy/core/base/bootstrap/VelocityBootstrap.java
+++ core-base/src/main/java/de/smoofy/core/base/bootstrap/VelocityBootstrap.java
@@ -7,6 +7,35 @@ package de.smoofy.core.base.bootstrap;
  * Created - 27.09.24, 23:46
  */
 
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import de.smoofy.core.base.CoreBase;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
+@Plugin(id = "SmoofyCore", name = "SmoofyCore", version = "1.0-SNAPSHOT", authors = {"Smoofy"})
 public class VelocityBootstrap {
 
+    @Getter
+    @Accessors(fluent = true)
+    private static VelocityBootstrap instance;
+
+    private final ProxyServer proxyServer;
+
+    @Inject
+    public VelocityBootstrap(ProxyServer proxyServer) {
+        this.proxyServer = proxyServer;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        instance = this;
+
+        new CoreBase(false);
+    }
 }
