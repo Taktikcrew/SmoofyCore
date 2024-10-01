83b99ee122ebd71f98ee7b068354df11374dbc46
Smoofy19 <nxcoop19@gmail.com>
2024-10-02 00:25:07 +0200

    Updated logger

--- core-base/src/main/java/de/smoofy/core/base/logger/LoggerImpl.java
+++ core-base/src/main/java/de/smoofy/core/base/logger/LoggerImpl.java
@@ -7,30 +7,35 @@ package de.smoofy.core.base.logger;
  * Created - 28.09.24, 00:02
  */
 
-import de.smoofy.core.api.Core;
 import de.smoofy.core.api.logger.ILogger;
 import de.smoofy.core.api.logger.enumeration.ConsoleColor;
 import de.smoofy.core.api.logger.enumeration.LogType;
-import de.smoofy.core.base.bootstrap.PaperBootstrap;
 import de.smoofy.core.base.bootstrap.VelocityBootstrap;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoggerImpl implements ILogger {
 
+    private final JavaPlugin plugin;
+
+    public LoggerImpl(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     public void log(LogType logType, String message) {
-        if (Core.instance().isPaper()) {
-            PaperBootstrap.instance().getLogger().info(ConsoleColor.GRAY + "[" + logType.consoleColor() +
-                    logType.label() + ConsoleColor.GRAY + "] » " + logType.consoleColor() + message);
+        if (this.plugin != null) {
+            this.plugin.getLogger().info(ConsoleColor.GRAY.code() + "[" + logType.consoleColor().code() +
+                    logType.label() + ConsoleColor.GRAY.code() + "] » " + logType.consoleColor().code() + message);
             return;
         }
-        VelocityBootstrap.instance().logger().info(ConsoleColor.GRAY + "[" + logType.consoleColor() +
-                logType.label() + ConsoleColor.GRAY + "] » " + logType.consoleColor() + message);
+        VelocityBootstrap.instance().logger().info(ConsoleColor.GRAY.code() + "[" + logType.consoleColor().code() +
+                logType.label() + ConsoleColor.GRAY.code() + "] » " + logType.consoleColor() + message);
     }
 
     @Override
     public void newLine() {
-        if (Core.instance().isPaper()) {
-            PaperBootstrap.instance().getLogger().info(" ");
+        if (this.plugin != null) {
+            this.plugin.getLogger().info(" ");
             return;
         }
         VelocityBootstrap.instance().logger().info(" ");
