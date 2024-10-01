33062bdba839dd3b8f47ab6923a6fa63e5dd718e
Smoofy19 <nxcoop19@gmail.com>
2024-10-02 00:26:45 +0200

    Updated logger

--- core-example/src/main/java/de/smoofy/core/examples/logger/LoggerExample.java
+++ core-example/src/main/java/de/smoofy/core/examples/logger/LoggerExample.java
@@ -8,16 +8,20 @@ package de.smoofy.core.examples.logger;
  */
 
 import de.smoofy.core.api.Core;
+import de.smoofy.core.api.logger.ILogger;
 import de.smoofy.core.api.logger.enumeration.LogType;
+import de.smoofy.core.examples.CoreExample;
 
 public class LoggerExample {
 
+    private final ILogger logger = Core.instance().logger(CoreExample.instance());
+
     public void test() {
-        Core.instance().logger().log(LogType.INFO, "This is a info log message.");
-        Core.instance().logger().newLine();
-        Core.instance().logger().log(LogType.ERROR, "This is a error log message.");
-        Core.instance().logger().log(LogType.WARNING, "This is a warning log message.");
-        Core.instance().logger().log(LogType.DEBUG, "This is a debug log message.");
+        this.logger.log(LogType.INFO, "This is a info log message.");
+        this.logger.newLine();
+        this.logger.log(LogType.ERROR, "This is a error log message.");
+        this.logger.log(LogType.WARNING, "This is a warning log message.");
+        this.logger.log(LogType.DEBUG, "This is a debug log message.");
     }
 
 }
