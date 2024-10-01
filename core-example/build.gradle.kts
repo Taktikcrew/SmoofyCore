0059d484c6b9f1ac5443e8a6e779812320ce5569
Smoofy19 <nxcoop19@gmail.com>
2024-10-02 00:25:48 +0200

    Added dependencies

--- core-example/build.gradle.kts
+++ core-example/build.gradle.kts
@@ -8,8 +8,21 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     mavenLocal()
+
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    implementation("de.smoofy.core:core-api:1.0-SNAPSHOT")
+    compileOnly("de.smoofy.core:core-api:1.0-SNAPSHOT")
+
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    sourceCompatibility = JavaVersion.VERSION_21.toString()
+    targetCompatibility = JavaVersion.VERSION_21.toString()
 }
