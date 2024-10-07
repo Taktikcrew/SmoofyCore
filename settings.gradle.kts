77e7f7962178adec691bec35f7a3e208b8340d70
Smoofy19 <nxcoop19@gmail.com>
2024-10-07 20:02:00 +0200

    Removed cloud adapter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,4 @@
 rootProject.name = "SmoofyCore"
 include("core-api")
 include("core-base")
-include("core-cloud:adapter-polocloud")
-findProject(":core-cloud:adapter-polocloud")?.name = "adapter-polocloud"
 include("core-example")
