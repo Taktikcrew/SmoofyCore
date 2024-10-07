fc48bd4738658aebf1bcf75da5b9a6b17da448fa
Smoofy19 <nxcoop19@gmail.com>
2024-10-07 18:51:04 +0200

    Added GitHub Package

--- core-api/build.gradle.kts
+++ core-api/build.gradle.kts
@@ -25,7 +25,20 @@ dependencies {
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Smoofy19/Core")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
     publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
         create<MavenPublication>("mavenJava") {
             from(components["java"])
         }
