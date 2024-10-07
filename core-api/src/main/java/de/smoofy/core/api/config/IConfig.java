27c7b42d4bb050f7db9088d5982dc64d0f4479b7
Smoofy19 <nxcoop19@gmail.com>
2024-10-07 14:58:13 +0200

    Added documentation

--- core-api/src/main/java/de/smoofy/core/api/config/IConfig.java
+++ core-api/src/main/java/de/smoofy/core/api/config/IConfig.java
@@ -9,18 +9,60 @@ package de.smoofy.core.api.config;
 
 public interface IConfig {
 
+    /**
+     * Gets a object from the config
+     *
+     * @param key the key
+     * @param clazz the class
+     * @return the object
+     */
     <T> T get(String key, Class<T> clazz);
 
-    IConfig addIfNotExists(String key, Object state);
+    /**
+     * Adds a value to the config if it does not exist
+     *
+     * @param key the key
+     * @param value the value
+     * @return the config
+     */
+    IConfig addIfNotExists(String key, Object value);
 
-    IConfig set(String key, Object object);
+    /**
+     * Sets a value in the config
+     *
+     * @param key the key
+     * @param value the value
+     * @return the config
+     */
+    IConfig set(String key, Object value);
 
+    /**
+     * Loads the config
+     *
+     * @return the config
+     */
     IConfig load();
 
+    /**
+     * Checks if the config has a key
+     *
+     * @param key the key
+     * @return true if the config has the key, false otherwise
+     */
     boolean has(String key);
 
+    /**
+     * Saves the config
+     *
+     * @return the config
+     */
     IConfig save();
 
+    /**
+     * Converts the config to a string
+     *
+     * @return the config as a string
+     */
     String toString();
 
 }
