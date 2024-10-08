0e20905ef8971101fca97dc7ac1f4e9616403d81
Smoofy19 <nxcoop19@gmail.com>
2024-10-08 17:09:33 +0200

    Use gradle plugin to create plugin.yml

--- core-base/build.gradle.kts
+++ core-base/build.gradle.kts
@@ -1,5 +1,8 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("net.minecrell.plugin-yml.paper") version "0.6.0"
     id("io.papermc.paperweight.userdev") version "1.7.3"
 }
 
@@ -33,4 +36,14 @@ tasks.shadowJar {
 
 tasks.assemble {
     dependsOn(tasks.reobfJar)
+}
+
+paper {
+    name = "Core"
+    version = project.version.toString()
+    author = "Smoofy"
+    apiVersion = "1.21"
+    main = "de.smoofy.core.base.bootstrap.PaperBootstrap"
+
+    load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
 }
